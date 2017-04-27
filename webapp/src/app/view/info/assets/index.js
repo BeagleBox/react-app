695d6e68057f8d99957cead14ef335c721078852
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2017-04-27 19:48:38 -0300

    Creating and styling info page

---
+++ webapp/src/app/view/info/assets/index.js
@@ -0,0 +1,11 @@
+import camera from './camera.jpg'
+import battery from './battery.png'
+import timeLeft from './time.png'
+import weight from './weight.png'
+
+export default {
+  camera,
+  battery,
+  timeLeft,
+  weight,
+}
