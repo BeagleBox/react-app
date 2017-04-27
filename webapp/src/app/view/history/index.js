bafdb25b843a9e56389b5396c5bd35783994adc6
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2017-04-27 15:01:59 -0300

    Filtering table results

--- webapp/src/app/view/history/index.js
+++ webapp/src/app/view/history/index.js
@@ -14,58 +14,71 @@ export default class History extends Component {
 
     this.state = {
       historyItems: [
-        {id: 1, date: '05/03/2017', origin: 'Secretaria', destiny: 'Sala I1', Checklist: ''},
-        {id: 2, date: '10/03/2017', origin: 'Secretaria', destiny: 'Biblioteca', Checklist: ''},
-        {id: 3, date: '15/03/2017', origin: 'Secretaria', destiny: 'Biblioteca', Checklist: ''},
-        {id: 4, date: '20/03/2017', origin: 'Secretaria', destiny: 'Enfermaria', Checklist: ''},
-        {id: 5, date: '25/03/2017', origin: 'Secretaria', destiny: 'Sala I8', Checklist: ''},
-        {id: 6, date: '30/03/2017', origin: 'Secretaria', destiny: 'Sala I6', Checklist: ''},
-        {id: 7, date: '05/04/2017', origin: 'Secretaria', destiny: 'Sala I2', Checklist: ''},
-      ]
+        {id: 1, date: '03-05-2017', origin: 'Secretaria', destiny: 'Sala I1', Checklist: ''},
+        {id: 2, date: '03-10-2017', origin: 'Secretaria', destiny: 'Biblioteca', Checklist: ''},
+        {id: 3, date: '03-15-2017', origin: 'Secretaria', destiny: 'Biblioteca', Checklist: ''},
+        {id: 4, date: '03-20-2017', origin: 'Secretaria', destiny: 'Enfermaria', Checklist: ''},
+        {id: 5, date: '03-25-2017', origin: 'Secretaria', destiny: 'Sala I8', Checklist: ''},
+        {id: 6, date: '03-30-2017', origin: 'Secretaria', destiny: 'Sala I6', Checklist: ''},
+        {id: 7, date: '04-05-2017', origin: 'Secretaria', destiny: 'Sala I2', Checklist: ''},
+      ],
+      itemsToAppear: [],
+      fromDate: new Date(+new Date() - 10 * 24 * 60 * 60 * 1000),
+      toDate: new Date(),
     }
+  }
+
+  validTable = (fromDate, toDate) => {
+    var items = this.state.historyItems;
+    var date = undefined;
+
+    this.setState({ itemsToAppear: [] })
+    for(var i = 0; i < items.length; i++) {
+      date = new Date(items[i].date).toISOString()
+
+      if(date >= fromDate.toISOString() && date <= toDate.toISOString()) {
+        this.setState({ itemsToAppear: this.state.itemsToAppear.concat([items[i]]) })
+      }
+    }
+  }
+
+  handleChangeFromDate = (fromDate) => {
+    this.setState({ fromDate })
+    this.validTable(fromDate, this.state.toDate)
+  }
+
+  handleChangeToDate = (toDate) => {
+    this.setState({ toDate })
+    this.validTable(this.state.fromDate, toDate)
   }
 
   tableContent = () => {
     return <TableBody displayRowCheckbox={false} >
-      {this.state.historyItems.map((item, k) =>
+      {this.state.itemsToAppear.map((item, k) =>
         <TableRow key={k} >
           <TableRowColumn className="row-center">{item.id}</TableRowColumn>
-          <TableRowColumn className="row-center">{item.date}</TableRowColumn>
+          <TableRowColumn className="row-center">{new Date(item.date).toLocaleDateString('pt-BR')}</TableRowColumn>
           <TableRowColumn>{item.origin}</TableRowColumn>
           <TableRowColumn>{item.destiny}</TableRowColumn>
           <TableRowColumn className="row-center">
             <RaisedButton label="Lista" primary={true} />
           </TableRowColumn>
         </TableRow>
-      )}
+      )};
     </TableBody>
   }
 
   render() {
     const DateTimeFormat = global.Intl.DateTimeFormat;
+    const noContent = this.state.itemsToAppear.length === 0;
 
     const styles = {
-      table: {
-        fontWeight: 'bold',
-        color: '#2F4F4F',
-      },
-      paper: {
-        width: '70%',
-        margin: 'auto',
-        padding: '2em',
-        maxHeight: '70vh',
-        overflowY: 'auto'
-      },
+      table: { fontWeight: 'bold', color: '#2F4F4F'},
+      paper: { width: '70%', margin: 'auto', padding: '2em', maxHeight: '70vh', overflowY: 'auto'},
     };
 
-    const dateProps = {
-      className: "filter-date",
-      mode: "landscape",
-      underlineShow: false,
-      autoOk: true,
-      okLabel: "OK",
-      cancelLabel: "Cancelar",
-      locale: "pt-BR",
+    const dateProps = { className: "filter-date", mode: "landscape", underlineShow: false, autoOk: true,
+      okLabel: "OK", cancelLabel: "Cancelar", locale: "pt-BR",
     };
 
     return (
@@ -84,12 +97,16 @@ export default class History extends Component {
                   <DatePicker
                     floatingLabelText="De..."
                     DateTimeFormat={DateTimeFormat}
+                    value={this.state.fromDate}
+                    onChange={(_, fromDate) => this.handleChangeFromDate(fromDate)}
                     {...dateProps} />
                 </Col>
                 <Col className="col-fluid col-filter" md={4} sm={6} xs={6}>
                   <DatePicker
                     floatingLabelText="Até..."
                     DateTimeFormat={DateTimeFormat}
+                    value={this.state.toDate}
+                    onChange={(_, toDate) => this.handleChangeToDate(toDate)}
                     {...dateProps} />
                 </Col>
               </Row>
@@ -97,18 +114,22 @@ export default class History extends Component {
           </Col>
           <Col className="col-fluid" md={12} sm={12} xs={12}>
             <Paper style={styles.paper} zDepth={2} >
-              <Table fixedHeader={true} selectable={false} >
-                <TableHeader adjustForCheckbox={false} displaySelectAll={false} >
-                  <TableRow >
-                    <TableHeaderColumn className="row-center" style={styles.table}>ID</TableHeaderColumn>
-                    <TableHeaderColumn className="row-center" style={styles.table}>Data</TableHeaderColumn>
-                    <TableHeaderColumn style={styles.table}>Origem</TableHeaderColumn>
-                    <TableHeaderColumn style={styles.table}>Destino</TableHeaderColumn>
-                    <TableHeaderColumn className="row-center" style={styles.table}>Carga</TableHeaderColumn>
-                  </TableRow>
-                </TableHeader>
-                { this.tableContent() }
-              </Table>
+              { noContent && <h4>Não há resultados</h4> }
+
+              { !noContent &&
+                <Table fixedHeader={true} selectable={false} >
+                  <TableHeader adjustForCheckbox={false} displaySelectAll={false} fixedHeader={true} >
+                    <TableRow >
+                      <TableHeaderColumn className="row-center" style={styles.table}>ID</TableHeaderColumn>
+                      <TableHeaderColumn className="row-center" style={styles.table}>Data</TableHeaderColumn>
+                      <TableHeaderColumn style={styles.table}>Origem</TableHeaderColumn>
+                      <TableHeaderColumn style={styles.table}>Destino</TableHeaderColumn>
+                      <TableHeaderColumn className="row-center" style={styles.table}>Carga</TableHeaderColumn>
+                    </TableRow>
+                  </TableHeader>
+                  { this.tableContent() }
+                </Table>
+              }
             </Paper>
           </Col>
         </Row>
