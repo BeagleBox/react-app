2fc2a15d9639a1a24dde1dcd911f88f1573aab47
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2017-04-27 02:07:27 -0300

    Adjusting table layout

--- webapp/src/app/view/history/index.js
+++ webapp/src/app/view/history/index.js
@@ -3,21 +3,54 @@ import { Grid, Row, Col } from 'react-flexbox-grid'
 
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table'
 import Paper from 'material-ui/Paper'
+import RaisedButton from 'material-ui/RaisedButton'
 
 import './history.css'
 
-export default class Request extends Component {
+export default class History extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      historyItems: [
+        {id: 1, origin: 'Secretaria', destiny: 'Sala I1', Checklist: ''},
+        {id: 2, origin: 'Secretaria', destiny: 'Biblioteca', Checklist: ''},
+        {id: 3, origin: 'Secretaria', destiny: 'Biblioteca', Checklist: ''},
+        {id: 4, origin: 'Secretaria', destiny: 'Enfermaria', Checklist: ''},
+        {id: 5, origin: 'Secretaria', destiny: 'Sala I8', Checklist: ''},
+        {id: 6, origin: 'Secretaria', destiny: 'Sala I6', Checklist: ''},
+        {id: 7, origin: 'Secretaria', destiny: 'Sala I2', Checklist: ''},
+        {id: 8, origin: 'Secretaria', destiny: 'Biblioteca', Checklist: ''},
+      ]
+    }
+  }
+
+  tableContent = () => {
+    return <TableBody displayRowCheckbox={false} >
+      {this.state.historyItems.map((item, k) =>
+        <TableRow>
+          <TableRowColumn>{item.id}</TableRowColumn>
+          <TableRowColumn>{item.origin}</TableRowColumn>
+          <TableRowColumn>{item.destiny}</TableRowColumn>
+          <TableRowColumn>
+            <RaisedButton label="Lista" primary={true} />
+          </TableRowColumn>
+        </TableRow>
+      )}
+    </TableBody>
+  }
+
   render() {
     const styles = {
       table: {
-        backgroundColor: '#F0F0F0',
         borderColor: '#000',
       },
       paper: {
         width: '70%',
         margin: 'auto',
-        padding: '1em',
-        backgroundColor: '#F0F0F0',
+        padding: '2em',
+        maxHeight: '80vh',
+        overflowY: 'auto'
       }
     };
 
@@ -25,11 +58,11 @@ export default class Request extends Component {
       <Grid className="history-container" fluid>
         <Row className="row-fluid history-content">
           <Col className="col-fluid" md={12} sm={12} xs={12}>
-            <h3>Meus Envios</h3>
+            <h3 className="history-title">Meus Envios</h3>
           </Col>
           <Col className="col-fluid" md={12} sm={12} xs={12}>
             <Paper style={styles.paper} zDepth={2} >
-              <Table fixedHeader={true} style={styles.table} >
+              <Table fixedHeader={true} style={styles.table} selectable={false} >
                 <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
                   <TableRow>
                     <TableHeaderColumn>ID</TableHeaderColumn>
@@ -38,32 +71,7 @@ export default class Request extends Component {
                     <TableHeaderColumn>Checklist</TableHeaderColumn>
                   </TableRow>
                 </TableHeader>
-                <TableBody displayRowCheckbox={false} >
-                  <TableRow>
-                    <TableRowColumn>1</TableRowColumn>
-                    <TableRowColumn>John Smith</TableRowColumn>
-                    <TableRowColumn>Employed</TableRowColumn>
-                    <TableRowColumn>Employed</TableRowColumn>
-                  </TableRow>
-                  <TableRow>
-                    <TableRowColumn>2</TableRowColumn>
-                    <TableRowColumn>Randal White</TableRowColumn>
-                    <TableRowColumn>Unemployed</TableRowColumn>
-                    <TableRowColumn>Employed</TableRowColumn>
-                  </TableRow>
-                  <TableRow>
-                    <TableRowColumn>3</TableRowColumn>
-                    <TableRowColumn>Stephanie Sanders</TableRowColumn>
-                    <TableRowColumn>Employed</TableRowColumn>
-                    <TableRowColumn>Employed</TableRowColumn>
-                  </TableRow>
-                  <TableRow>
-                    <TableRowColumn>4</TableRowColumn>
-                    <TableRowColumn>Steve Brown</TableRowColumn>
-                    <TableRowColumn>Employed</TableRowColumn>
-                    <TableRowColumn>Employed</TableRowColumn>
-                  </TableRow>
-                </TableBody>
+                { this.tableContent() }
               </Table>
             </Paper>
           </Col>
