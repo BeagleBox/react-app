d3096a4d7dd010f92e813e2c885ecb08b27723c6
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2017-04-27 22:43:23 -0300

    Adding home to appbar

--- webapp/src/app/shared/app-bar/index.js
+++ webapp/src/app/shared/app-bar/index.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react'
 
-import RaisedButton from 'material-ui/RaisedButton'
+import FlatButton from 'material-ui/FlatButton'
+import IconButton from 'material-ui/IconButton'
+import Home from 'material-ui/svg-icons/action/home'
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar'
 
 import './app-bar.css'
 
 export default class Homepage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {
+        name: 'Elaine Meirelles'
+      },
+    }
+  }
+
   render() {
     return (
-      <Toolbar style={{backgroundColor: '#808080'}}>
-        <ToolbarGroup firstChild={true} >
-          <ToolbarTitle className="toolbar-title" text="Olá, Secretaria" />
-        </ToolbarGroup>
-        <ToolbarGroup>
-          <RaisedButton label="Solicitar" default={true} />
-          <ToolbarSeparator className="separator" />
-          <RaisedButton label="Receber" default={true} />
-          <ToolbarSeparator className="separator" />
-          <RaisedButton label="Histórico" default={true} />
-        </ToolbarGroup>
-      </Toolbar>
+      <div>
+        {this.state.user.name !== '' &&
+          <Toolbar style={{backgroundColor: '#808080'}}>
+            <ToolbarGroup firstChild={true} >
+              <ToolbarTitle className="toolbar-title" text={"Olá, " + this.state.user.name} />
+            </ToolbarGroup>
+            <ToolbarGroup>
+              <IconButton href="/#/informacoes-gerais">
+                <Home />
+              </IconButton>
+              <ToolbarSeparator className="separator" />
+              <FlatButton
+                backgroundColor="#CCC"
+                hoverColor="#DCDCDC"
+                label="Solicitar"
+                href="/#/solicitar" />
+              <ToolbarSeparator className="separator" />
+              <FlatButton
+                backgroundColor="#CCC"
+                hoverColor="#DCDCDC"
+                label="Receber"
+                href="/#/receber" />
+              <ToolbarSeparator className="separator" />
+              <FlatButton
+                backgroundColor="#CCC"
+                hoverColor="#DCDCDC"
+                label="Histórico"
+                href="/#/historico" />
+            </ToolbarGroup>
+          </Toolbar>
+        }
+      </div>
     );
   }
 }
