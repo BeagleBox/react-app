0502c16a335e63644ba97c3099bf586a629fb9c7
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2017-05-09 09:25:36 -0300

    Removing duplication at history table

---
+++ webapp/src/app/shared/transportation-table/index.js
@@ -0,0 +1,18 @@
+import { connect } from "react-redux"
+import TransportationTableComponent from "./transportation-table"
+
+const stateToProps = (state) => {
+  return {
+
+  }
+};
+
+const dispatchToProps = (dispatch) => {
+  return {
+
+  }
+};
+
+const TransportationTable = connect(stateToProps, dispatchToProps)(TransportationTableComponent);
+
+export default TransportationTable;
